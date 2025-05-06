ec7b6366eafbc1a957c729308196805495b47b58
m4rreParre <marrebjoredal@gmail.com>
2025-05-06 21:58:20 +0200

    Add initial project files and configurations for Practice02
    - Created Practice02.dll and Practice02.refint.dll files for the project.
    - Added project.assets.json to manage package dependencies.
    - Included project configuration files: Practice02.csproj.nuget.dgspec.json, Practice02.csproj.nuget.g.props, Practice02.csproj.nuget.g.targets.
    - Established solution file for the project structure.
    - Configured NuGet package sources and restore settings.
    - Defined assembly attributes and trust information in the assembly manifest.

---
+++ Practice02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Practice02
+{
+    class Program
+    {
+        static public int[,] SumList = new int[,]
+        {
+            {5, 5, 5, 4}, //adds up to 19
+            {8, 6, 2, 4}, //adds up to 20
+            {5, 1, 2, 5}, //adds up to 13
+            {4, 5, 4, 6}, //adds up to 19
+            {4, 4, 4, 4}, //adds up to 16
+        }; 
+
+        static int AddRad(int[,] list, int n)
+        {
+            n--;
+            bool lengthIsAccepted = n >= 0 && n < list.GetLength(0);
+            int sum = 0;
+            if(lengthIsAccepted)
+            {
+                for(int i = 0; i < list.GetLength(1); i++)
+                {
+                    sum += list[n, i];
+                }
+                return sum;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int output = AddRad(SumList, 1);
+            Console.WriteLine(output);
+        }
+    }
+}
